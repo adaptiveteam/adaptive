fdfb8f6128ca79bcb7028d0e0a29b39741ef0c9c
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-01-08 16:14:45 +0300

    Remove localstack initialization from tests

--- aws-utils-go/client_suite_test.go
+++ aws-utils-go/client_suite_test.go
@@ -1,14 +1,14 @@
 package aws_utils_go
 
 import (
-	"time"
+	// "time"
 	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/ory/dockertest"
-	dc "github.com/ory/dockertest/docker"
+	// dc "github.com/ory/dockertest/docker"
 	"github.com/stretchr/testify/assert"
-	"os"
+	// "os"
 	"testing"
 )
 
@@ -35,61 +35,61 @@ func TestBooks(t *testing.T) {
 	RunSpecs(t, "Integration Test Suite")
 }
 
-// Running this once before the test suite
-var _ = BeforeSuite(func() {
-	fmt.Println("Starting localstack container ... ")
-	resourcePool, err = dockertest.NewPool("")
-	testErrorHandler(err, "Could not connect to docker")
-	// Starting localstack docker container with port mappings
-	// Lambdas in golang require 'LAMBDA_EXECUTOR=docker'
-	// Privileged access is required to start docker inside the container
-	resource, err = resourcePool.RunWithOptions(
-		&dockertest.RunOptions{
-			Repository: "localstack/localstack",
-			Tag:        "0.10.5",
-			PortBindings: map[dc.Port][]dc.PortBinding{
-				// S3
-				"4572/tcp": {{HostIP: "localhost", HostPort: "4572"}},
-				// Dynamodb
-				"4569/tcp": {{HostIP: "localhost", HostPort: "4569"}},
-				// Lambda
-				"4574/tcp": {{HostIP: "localhost", HostPort: "4574"}},
-				// SNS
-				"4575/tcp": {{HostIP: "localhost", HostPort: "4575"}},
-			},
-			// Env should be []string{} for python lambdas
-			// should be []string{"LAMBDA_EXECUTOR=docker"}, for non-python lambdas
-			Env:        []string{"LAMBDA_EXECUTOR=docker", "DEBUG=1"},
-			Privileged: true,
-		},
-	)
-	testErrorHandler(err, "Could not start docker container")
+// // Running this once before the test suite
+// var _ = BeforeSuite(func() {
+// 	fmt.Println("Starting localstack container ... ")
+// 	resourcePool, err = dockertest.NewPool("")
+// 	testErrorHandler(err, "Could not connect to docker")
+// 	// Starting localstack docker container with port mappings
+// 	// Lambdas in golang require 'LAMBDA_EXECUTOR=docker'
+// 	// Privileged access is required to start docker inside the container
+// 	resource, err = resourcePool.RunWithOptions(
+// 		&dockertest.RunOptions{
+// 			Repository: "localstack/localstack",
+// 			Tag:        "0.10.5",
+// 			PortBindings: map[dc.Port][]dc.PortBinding{
+// 				// S3
+// 				"4572/tcp": {{HostIP: "localhost", HostPort: "4572"}},
+// 				// Dynamodb
+// 				"4569/tcp": {{HostIP: "localhost", HostPort: "4569"}},
+// 				// Lambda
+// 				"4574/tcp": {{HostIP: "localhost", HostPort: "4574"}},
+// 				// SNS
+// 				"4575/tcp": {{HostIP: "localhost", HostPort: "4575"}},
+// 			},
+// 			// Env should be []string{} for python lambdas
+// 			// should be []string{"LAMBDA_EXECUTOR=docker"}, for non-python lambdas
+// 			Env:        []string{"LAMBDA_EXECUTOR=docker", "DEBUG=1"},
+// 			Privileged: true,
+// 		},
+// 	)
+// 	testErrorHandler(err, "Could not start docker container")
 
-	s := NewS3(os.Getenv("AWS_REGION"), fmt.Sprintf("http://%s:4572", hostname), "test")
+// 	s := NewS3(os.Getenv("AWS_REGION"), fmt.Sprintf("http://%s:4572", hostname), "test")
 
-	// Ensuring container is ready to accept requests
-	if err = resourcePool.Retry(func() error {
-		_, err = s.ListBuckets()
-		return err
-	}); err != nil {
-		testErrorHandler(err, "Could not connect to docker")
-	}
-	time.Sleep(10*time.Second)
-	fmt.Println("Started localstack container ... ")
-})
+// 	// Ensuring container is ready to accept requests
+// 	if err = resourcePool.Retry(func() error {
+// 		_, err = s.ListBuckets()
+// 		return err
+// 	}); err != nil {
+// 		testErrorHandler(err, "Could not connect to docker")
+// 	}
+// 	time.Sleep(10*time.Second)
+// 	fmt.Println("Started localstack container ... ")
+// })
 
-// Running this once after the test suite
-var _ = AfterSuite(func() {
-	fmt.Println("Stopping localstack container ... ")
-	// Once tests are done, kill and remove the container
-	if resource != nil {
-		err1 := resource.Expire(5)
-		testErrorHandler(err1, "There was an error with expiring localstack resource")
-		err2 := resource.Close()
-		testErrorHandler(err2, "There was in error with closing localstack resource")
-		// err3 := resourcePool.Purge(resource)
-		// testErrorHandler(err3, "There was in error with stopping the container")
-	}
-	time.Sleep(30*time.Second)// to free up ports
-	fmt.Println("Stopped localstack container")
-})
+// // Running this once after the test suite
+// var _ = AfterSuite(func() {
+// 	fmt.Println("Stopping localstack container ... ")
+// 	// Once tests are done, kill and remove the container
+// 	if resource != nil {
+// 		err1 := resource.Expire(5)
+// 		testErrorHandler(err1, "There was an error with expiring localstack resource")
+// 		err2 := resource.Close()
+// 		testErrorHandler(err2, "There was in error with closing localstack resource")
+// 		// err3 := resourcePool.Purge(resource)
+// 		// testErrorHandler(err3, "There was in error with stopping the container")
+// 	}
+// 	time.Sleep(30*time.Second)// to free up ports
+// 	fmt.Println("Stopped localstack container")
+// })
--- dialog-fetcher/dialog_fetcher_suite_test.go
+++ dialog-fetcher/dialog_fetcher_suite_test.go
@@ -3,16 +3,16 @@ package fetch_dialog
 import (
 	"fmt"
 	"testing"
-	"time"
+	// "time"
 
 	awsutils "github.com/adaptiveteam/adaptive/aws-utils-go"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/ory/dockertest"
-	"github.com/ory/dockertest/docker"
+	// "github.com/ory/dockertest/docker"
 	"github.com/stretchr/testify/assert"
 
-	. "github.com/adaptiveteam/adaptive/aws-utils-go"
+	// . "github.com/adaptiveteam/adaptive/aws-utils-go"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
@@ -46,73 +46,73 @@ func testErrorHandler(err error, msg string) {
 
 // Running this once before the test suite
 var _ = BeforeSuite(func() {
-	var err error
-	fmt.Println("Starting localstack container ... ")
-	resourcePool, err = dockertest.NewPool("")
-	testErrorHandler(err, "Could not connect to docker")
-	// Starting localstack docker container with port mappings
-	// Lambdas in golang require 'LAMBDA_EXECUTOR=docker'
-	// Privileged access is required to start docker inside the container
-	resource, err = resourcePool.RunWithOptions(
-		&dockertest.RunOptions{
-			Repository: "localstack/localstack",
-			Tag:        "0.10.5",
-			PortBindings: map[docker.Port][]docker.PortBinding{
-				// S3
-				"4572/tcp": {{HostIP: hostname, HostPort: "4572"}},
-				// Dynamodb
-				"4569/tcp": {{HostIP: hostname, HostPort: "4569"}},
-				// Lambda
-				"4574/tcp": {{HostIP: hostname, HostPort: "4574"}},
-				// SNS
-				"4575/tcp": {{HostIP: hostname, HostPort: "4575"}},
-			},
-			// Env should be []string{} for python lambdas
-			// should be []string{"LAMBDA_EXECUTOR=docker"}, for non-python lambdas
-			Env:        []string{"LAMBDA_EXECUTOR=docker", "DEBUG=1"},
-			Privileged: true,
-		},
-	)
-	if err != nil {
-		fmt.Printf("Ignoring error when creating docker pool: %+v", err)
-		resource = nil
-	}
-	// testErrorHandler(err, "Could not start docker container")
+	// var err error
+	// fmt.Println("Starting localstack container ... ")
+	// resourcePool, err = dockertest.NewPool("")
+	// testErrorHandler(err, "Could not connect to docker")
+	// // Starting localstack docker container with port mappings
+	// // Lambdas in golang require 'LAMBDA_EXECUTOR=docker'
+	// // Privileged access is required to start docker inside the container
+	// resource, err = resourcePool.RunWithOptions(
+	// 	&dockertest.RunOptions{
+	// 		Repository: "localstack/localstack",
+	// 		Tag:        "0.10.5",
+	// 		PortBindings: map[docker.Port][]docker.PortBinding{
+	// 			// S3
+	// 			"4572/tcp": {{HostIP: hostname, HostPort: "4572"}},
+	// 			// Dynamodb
+	// 			"4569/tcp": {{HostIP: hostname, HostPort: "4569"}},
+	// 			// Lambda
+	// 			"4574/tcp": {{HostIP: hostname, HostPort: "4574"}},
+	// 			// SNS
+	// 			"4575/tcp": {{HostIP: hostname, HostPort: "4575"}},
+	// 		},
+	// 		// Env should be []string{} for python lambdas
+	// 		// should be []string{"LAMBDA_EXECUTOR=docker"}, for non-python lambdas
+	// 		Env:        []string{"LAMBDA_EXECUTOR=docker", "DEBUG=1"},
+	// 		Privileged: true,
+	// 	},
+	// )
+	// if err != nil {
+	// 	fmt.Printf("Ignoring error when creating docker pool: %+v", err)
+	// 	resource = nil
+	// }
+	// // testErrorHandler(err, "Could not start docker container")
 
-	// Ensuring container is ready to accept requests
-	if err = resourcePool.Retry(func() error {
-		s := NewS3(testAwsRegion(), fmt.Sprintf("http://%s:4572", hostname), "test")
-		_, err = s.ListBuckets()
-		return err
-	}); err != nil {
-		testErrorHandler(err, "Could not connect to docker")
-	}
-	time.Sleep(10*time.Second)
+	// // Ensuring container is ready to accept requests
+	// if err = resourcePool.Retry(func() error {
+	// 	s := NewS3(testAwsRegion(), fmt.Sprintf("http://%s:4572", hostname), "test")
+	// 	_, err = s.ListBuckets()
+	// 	return err
+	// }); err != nil {
+	// 	testErrorHandler(err, "Could not connect to docker")
+	// }
+	// time.Sleep(10*time.Second)
 
-	fmt.Println("Started localstack container")
+	// fmt.Println("Started localstack container")
 	addDialogSchema()
-	err = addMockData(localStackDao())
-	if err != nil {
-		testingT.Errorf("Problem creating mock data: %v", err)
+	err2 := addMockData(localStackDao())
+	if err2 != nil {
+		testingT.Errorf("Problem creating mock data: %+v", err2)
 	}
 })
 
-// Running this once after the test suite
-var _ = AfterSuite(func() {
-	fmt.Println("Stopping localstack container ... ")
-	// Once tests are done, kill and remove the container
-	if resourcePool == nil {
-		panic("No resource pool")
-	}
-	if resource != nil {
-		err2 := resource.Close()
-		testErrorHandler(err2, "There was an error with closing localstack resource")
+// // Running this once after the test suite
+// var _ = AfterSuite(func() {
+// 	fmt.Println("Stopping localstack container ... ")
+// 	// Once tests are done, kill and remove the container
+// 	if resourcePool == nil {
+// 		panic("No resource pool")
+// 	}
+// 	if resource != nil {
+// 		err2 := resource.Close()
+// 		testErrorHandler(err2, "There was an error with closing localstack resource")
 
-		// err3 := resourcePool.Purge(resource)
-		// testErrorHandler(err3, "There was an error with stopping the container")
-	}
-	fmt.Println("Stopped localstack container")
-})
+// 		// err3 := resourcePool.Purge(resource)
+// 		// testErrorHandler(err3, "There was an error with stopping the container")
+// 	}
+// 	fmt.Println("Stopped localstack container")
+// })
 
 func localStackDynamoDB() *dynamodb.DynamoDB {
 	sess := session.Must(session.NewSession(&aws.Config{
