4cf3ada5db36fbb3877ee461274321c4a6c54ba9
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-01-08 10:45:56 +0300

    Move check function names

---
+++ adaptive-check-function-names/function-names.go
@@ -0,0 +1,67 @@
+package adaptive_check_function_names
+
+/* For every check function added it's name must be included in this list */
+const (
+	// Feedback tests
+	FeedbackGivenThisQuarter = "FeedbackGivenThisQuarter"
+	InLastMonthOfQuarter     = "InLastMonthOfQuarter"
+
+	CoacheesExist                             = "CoacheesExist"
+	AdvocatesExist                            = "AdvocatesExist"
+
+	// Due date tests
+	IDOsDueWithinTheWeek    = "IDOsDueWithinTheWeek"
+	IDOsDueWithinTheMonth   = "IDOsDueWithinTheMonth"
+	IDOsDueWithinTheQuarter = "IDOsDueWithinTheQuarter"
+
+	InitiativesDueWithinTheWeek    = "InitiativesDueWithinTheWeek"
+	InitiativesDueWithinTheMonth   = "InitiativesDueWithinTheMonth"
+	InitiativesDueWithinTheQuarter = "InitiativesDueWithinTheQuarter"
+
+	ObjectivesDueWithinTheWeek    = "ObjectiveDueWithinTheWeek"
+	ObjectivesDueWithinTheMonth   = "ObjectiveDueWithinTheMonth"
+	ObjectivesDueWithinTheQuarter = "ObjectiveDueWithinTheQuarter"
+
+	// Community membership tests
+	InCapabilityCommunity = "InCapabilityCommunity"
+	InValuesCommunity     = "InValuesCommunity"
+	InHRCommunity         = "InHRCommunity"
+	InStrategyCommunity   = "InStrategyCommunity"
+	InInitiativeCommunity = "InInitiativeCommunity"
+
+	// Component existence tests
+
+	// Miscellaneous
+	UserSettingsExist                = "UserSettingsExist"
+	HolidaysExist                    = "HolidaysExist"
+	CollaborationReportExists        = "CollaborationReportExists"
+	UndeliveredEngagementsExistForMe = "UndeliveredEngagementsExistForMe"
+	CanBeNudgedForIDO                = "CanBeNudgedForIDO"
+
+	// Strategy component existence tests independent of the user
+	TeamValuesExist     = "TeamValuesExist"
+	CompanyVisionExists = "CompanyVisionExists"
+	ObjectivesExist     = "ObjectivesExist"
+	InitiativesExist    = "InitiativesExist"
+
+	// Strategy component existence tests for a given user
+	IDOsExistForMe        = "IDOsExistForMe"
+	ObjectivesExistForMe  = "ObjectivesExistForMe"
+	InitiativesExistForMe = "InitiativesExistForMe"
+
+	// Stale components exist for a specfc individual
+	StaleIDOsExistForMe        = "StaleIDOsExistForMe"
+	StaleInitiativesExistForMe = "StaleInitiativesExistForMe"
+	StaleObjectivesExistForMe  = "StaleObjectivesExistForMe"
+
+	// Community existence tests
+	CapabilityCommunityExists           = "CapabilityCommunityExists"
+	MultipleCapabilityCommunitiesExists = "MultipleCapabilityCommunitiesExists"
+	InitiativeCommunityExists           = "InitiativeCommunityExists"
+	MultipleInitiativeCommunitiesExists = "MultipleInitiativeCommunitiesExists"
+
+	// State of community tests
+	ObjectivesExistInMyCapabilityCommunities  = "ObjectivesExistInMyCapabilityCommunities"
+	InitiativesExistInMyCapabilityCommunities = "InitiativesExistInMyCapabilityCommunities"
+	InitiativesExistInMyInitiativeCommunities = "InitiativesExistInMyInitiativeCommunities"
+)
