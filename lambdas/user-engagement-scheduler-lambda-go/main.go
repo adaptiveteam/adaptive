cef06f528d9078a95ec4737ee0de9718950566a1
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-01-23 13:02:08 +0300

    User core error handling

--- lambdas/user-engagement-scheduler-lambda-go/main.go
+++ lambdas/user-engagement-scheduler-lambda-go/main.go
@@ -1,8 +1,6 @@
 package lambda
 
 import (
-	"github.com/sirupsen/logrus"
-	alog "github.com/adaptiveteam/adaptive/adaptive-utils-go/logger"
 	"context"
 	"fmt"
 	aesc "github.com/adaptiveteam/adaptive/adaptive-engagement-scheduling/common"
@@ -29,24 +27,9 @@ var (
 	userDao  = utilsUser.NewDAOFromSchema(d, namespace, schema)
 )
 
-func handleError(err *error){
-	if *err != nil {
-		alog.LambdaLogger(logrus.InfoLevel).WithError(*err).Errorf("Ordinary error in user-engagement-scheduler-lambda")
-	}
-	
-	if err2 := recover(); err2 != nil {
-		err3, ok := err2.(error)
-		err4 := fmt.Errorf("panic-error in user-engagement-scheduler-lambda %+v", err2)
-		err = &err4
-		if ok {
-			alog.LambdaLogger(logrus.InfoLevel).WithError(err3).Errorf("Panic error in user-engagement-scheduler-lambda")
-		} else {
-			alog.LambdaLogger(logrus.InfoLevel).WithField("non-error", err2).Errorf("Unusual panic error in user-engagement-scheduler-lambda")
-		}
-	}
-}
+// HandleRequest -
 func HandleRequest(ctx context.Context, event EngSchedule) (err error) {
-	defer handleError(&err)
+	defer core.RecoverToErrorVar("user-engagement-scheduler-lambda-go", &err)
 	var t time.Time
 	if event.Date != "" {
 		fmt.Printf("Date is present in EngSchedule")
