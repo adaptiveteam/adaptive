949fa7dc0bf81d7b90596d4bb6b7e2f216569261
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-01-29 11:09:03 +0300

    Deploy adaptive binary to an s3 bucket

--- infra/core/terraform/adaptive_binary.tf
+++ infra/core/terraform/adaptive_binary.tf
@@ -3,3 +3,26 @@ data "archive_file" "adaptive-lambda-zip" {
   source_file = "../../../bin/adaptive"
   output_path = "lambdas/adaptive.zip"
 }
+
+resource "aws_s3_bucket" "adaptive-binary-bucket" {
+  bucket = "${local.client_id}-binary"
+  acl    = "private"
+  region = local.region
+
+  versioning {
+    enabled = true
+  }
+
+  tags = local.default_tags
+}
+
+resource "aws_s3_bucket_object" "adaptive_binary" {
+  bucket = aws_s3_bucket.adaptive-binary-bucket.bucket
+  key    = "adaptive.zip"
+  source = data.archive_file.adaptive-lambda-zip.output_path
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(data.archive_file.adaptive-lambda-zip.output_path)
+}
