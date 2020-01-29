66562c9b9768cb7a85c519999a3d281d056dbaa6
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-01-29 11:08:38 +0300

    Separate aws-provider info

---
+++ infra/core/terraform/aws.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
+terraform {
+  required_version = ">= 0.12"
+}
--- infra/core/terraform/main.tf
+++ infra/core/terraform/main.tf
@@ -1,11 +1,4 @@
 # Specify the provider and access details
-provider "aws" {
-  region = var.aws_region
-}
-
-data "aws_caller_identity" "current" {
-}
-
 locals {
   //  adaptive_values_table_name = "${var.client_id}_adaptive_values"
   //  dynamo_adaptive_values_platform_id_index = "AdaptiveValuesPlatformIdIndex"
