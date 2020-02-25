7cb46ed296a4d1756ac243afe796d6187b83becc
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-02-25 18:50:26 +0300

    Use TeamID

--- lambdas/adaptive-platform-notification-lambda-go/environment.go
+++ lambdas/adaptive-platform-notification-lambda-go/environment.go
@@ -3,12 +3,8 @@ package lambda
 import (
 	utils "github.com/adaptiveteam/adaptive/adaptive-utils-go"
 	models "github.com/adaptiveteam/adaptive/adaptive-utils-go/models"
-	plat "github.com/adaptiveteam/adaptive/adaptive-utils-go/platform"
 	"github.com/adaptiveteam/adaptive/adaptive-utils-go/user"
 	awsutils "github.com/adaptiveteam/adaptive/aws-utils-go"
-	mapper "github.com/adaptiveteam/adaptive/engagement-slack-mapper"
-	// "github.com/adaptiveteam/adaptive/adaptive-engagements/common"
-	// dialogFetcher "github.com/adaptiveteam/adaptive/dialog-fetcher"
 )
 
 var (
@@ -21,8 +17,6 @@ var (
 	d      = awsutils.NewDynamo(region, "", namespace)
 	schema = models.SchemaForClientID(clientID)
 
-	platformTokenDao = plat.NewDAOFromSchema(d, namespace, schema)
-	platformAdapter  = mapper.SlackAdapter2(platformTokenDao)
 	// instead of profile lambda
 	userDao = user.NewDAOFromSchema(d, namespace, schema)
 )
