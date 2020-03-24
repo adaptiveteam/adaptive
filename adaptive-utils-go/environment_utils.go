3f0a43a0d920f598bfa86eb3e52402ec41bfafee
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-03-24 15:50:30 +0300

    Move NonEmptyEnv to `core`

--- adaptive-utils-go/environment_utils.go
+++ adaptive-utils-go/environment_utils.go
@@ -1,28 +1,10 @@
 package adaptive_utils_go
 
 import (
-	"github.com/pkg/errors"
-	"os"
+	core "github.com/adaptiveteam/adaptive/core-utils-go"
 )
-
 
 var (
 	// NonEmptyEnv is the environment that is checked for emptiness
-	NonEmptyEnv = NonEmptyMap(os.Getenv)
+	NonEmptyEnv = core.NonEmptyEnv
 )
-// GetNonEmptyEnv reads a single value from non emtpy environment
-func GetNonEmptyEnv(name string) string {
-	return NonEmptyEnv(name)
-}
-
-// NonEmptyMap is a function that converts a mapping function to 
-// mapping function that will check value for emptiness
-func NonEmptyMap(env func(string)string) func(string)string {
-	return func(key string)string {
-		value := env(key)
-		if value == "" {
-			panic(errors.New("Key " + key + " is not defined"))
-		}
-		return value
-	}
-}
