726867b159a743ebbd7c7e1d71d737ad7fca6421
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-03-27 10:26:28 +0300

    Implement generic InterfaceQueryPager

--- adaptive-engagements/strategy/objectives-stream.go
+++ adaptive-engagements/strategy/objectives-stream.go
@@ -36,27 +36,48 @@ func AsInterfaceSlice(sos []strategyObjective.StrategyObjective) (res pagination
 	return
 
 }
+// DynamoDBKey - key
+type DynamoDBKey = map[string]*dynamodb.AttributeValue
+// KeyExtractor obtains key value from the given instance
+type KeyExtractor = func(interface{}) DynamoDBKey
 
-func readByPlatformIDPager(conn daosCommon.DynamoDBConnection, queryInput dynamodb.QueryInput) func()  (sl pagination.InterfaceSlice, ip pagination.InterfacePager, err error) {
+// StrategyObjectiveKey retrieves key of the instance
+func StrategyObjectiveKey(i interface{}) DynamoDBKey {
+	so := i.(strategyObjective.StrategyObjective)
+	return map[string]*dynamodb.AttributeValue{ 
+		string(strategyObjective.PlatformID): daosCommon.DynS(string(so.PlatformID)),
+		string(strategyObjective.ID):         daosCommon.DynS(so.ID),
+	}
+}
+// InterfaceQueryPager constructs a pager that will be yielding pages using 
+// DynamoDB Query.
+// sliceOfEntities = []models.StrategyObjective
+// asInterfaceSlice = AsInterfaceSlice
+// keyExtractor = StrategyObjectiveKey
+// TODO: combine these into a type class
+func InterfaceQueryPager(
+	conn daosCommon.DynamoDBConnection, 
+	queryInput dynamodb.QueryInput,
+	sliceOfEntities interface{},
+	asInterfaceSlice func (interface{}) pagination.InterfaceSlice,
+	keyExtractor KeyExtractor,
+	) pagination.InterfacePager {
 	return func() (sl pagination.InterfaceSlice, ip pagination.InterfacePager, err error) {
 		if queryInput.Limit == nil || *queryInput.Limit > 0 {
-			var instances []models.StrategyObjective
-			err = conn.Dynamo.QueryInternal(queryInput, &instances)
+			// var instances []models.StrategyObjective
+			err = conn.Dynamo.QueryInternal(queryInput, sliceOfEntities)
 			if err == nil {
-				sl = AsInterfaceSlice(instances)
+				sl = asInterfaceSlice(sliceOfEntities)
 				if len(sl) > 0 {
-					last := instances[len(instances) - 1]
-					queryInput.ExclusiveStartKey = map[string]*dynamodb.AttributeValue{ 
-						string(strategyObjective.PlatformID): daosCommon.DynS(string(last.PlatformID)),
-						string(strategyObjective.ID): daosCommon.DynS(last.ID),
-					}
+					last := sl[len(sl) - 1]
+					queryInput.ExclusiveStartKey = keyExtractor(last)
 				}
 				if queryInput.Limit != nil {
-					if int64(len(instances)) > *queryInput.Limit {
+					if int64(len(sl)) > *queryInput.Limit {
 						sl = sl[0:*queryInput.Limit]
 						*queryInput.Limit = 0
 					} else {
-						*queryInput.Limit = *queryInput.Limit - int64(len(instances))
+						*queryInput.Limit = *queryInput.Limit - int64(len(sl))
 					}
 				}
 				
@@ -69,6 +90,15 @@ func readByPlatformIDPager(conn daosCommon.DynamoDBConnection, queryInput dynamo
 		}
 		return
 	}
+}
+
+func readByPlatformIDPager(conn daosCommon.DynamoDBConnection, queryInput dynamodb.QueryInput) func()  (sl pagination.InterfaceSlice, ip pagination.InterfacePager, err error) {
+	var instances []models.StrategyObjective
+	return InterfaceQueryPager(conn, queryInput,
+		instances,
+		func (i interface{}) pagination.InterfaceSlice {return  AsInterfaceSlice(i.([]models.StrategyObjective))},
+		StrategyObjectiveKey,
+	)
 }
 // StrategyObjective_ReadByPlatformIDStream create stream that will 
 // read all strategy objectives for platform id
@@ -101,10 +131,6 @@ func SelectFromStrategyObjectiveJoinUserObjectiveWhereNotCompletedStream() (stm 
 		return func (i interface{}) (res bool, err error) {
 			so := i.(strategyObjective.StrategyObjective)
 			return IsStrategyObjectiveNotCompleted(so)(conn)
-			// var uos []userObjective.UserObjective
-			// uos, err = userObjective.ReadOrEmpty(so.ID)(conn)
-			// res = len(uos) > 0 && uos[0].Completed == 0 
-			// return
 		}
 	})
 }
