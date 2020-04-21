42797261e719e2db8253ede7ce7876fa73110635
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-04-21 16:52:35 +0300

    Use S3 terraform module

--- infra/core/terraform/lambda_slack_message_processor.tf
+++ infra/core/terraform/lambda_slack_message_processor.tf
@@ -3,11 +3,12 @@ locals {
   slack_message_processor_lambda_function_name = "${var.client_id}_${local.slack_message_processor_lambda_function_name_suffix}"
 }
 module "slack_message_processor_lambda" {
-  source = "../../../terraform-modules/adaptive-lambda"
+  source = "../../../terraform-modules/adaptive-lambda-s3"
+  s3_bucket = aws_s3_bucket.binary_bucket.bucket
+  s3_key = aws_s3_bucket_object.adaptive_zip.key
+  source_hash = data.archive_file.adaptive_lambda_zip.output_md5
 
   client_id = var.client_id
-  filename = data.archive_file.adaptive-lambda-zip.output_path
-  source_hash = data.archive_file.adaptive-lambda-zip.output_base64sha256
   handler = "adaptive"
   function_name_suffix = local.slack_message_processor_lambda_function_name_suffix
   runtime = var.lambda_runtime
--- infra/core/terraform/lambda_user_query.tf
+++ infra/core/terraform/lambda_user_query.tf
@@ -1,9 +1,10 @@
 module "user_query_lambda" {
-  source = "../../../terraform-modules/adaptive-lambda"
+  source = "../../../terraform-modules/adaptive-lambda-s3"
+  s3_bucket = aws_s3_bucket.binary_bucket.bucket
+  s3_key = aws_s3_bucket_object.adaptive_zip.key
+  source_hash = data.archive_file.adaptive_lambda_zip.output_md5
 
   client_id     = var.client_id
-  filename      = data.archive_file.adaptive-lambda-zip.output_path
-  source_hash   = data.archive_file.adaptive-lambda-zip.output_base64sha256
   handler       = "adaptive"
   function_name_suffix = "user-query-lambda-go"
   runtime       = var.lambda_runtime
