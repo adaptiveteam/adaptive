ccea48bd5ce9da7f654a804db6acadd8c86138ce
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-04-21 17:06:15 +0300

    Upload file script

---
+++ migrations/upload-file/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	token := "xoxp-4..."
+	api := slack.New(token)
+
+	buf := bytes.NewBufferString("hello")
+	params := slack.FileUploadParameters{
+		Title:           "Simple file",
+		Filename:        "file.txt",
+		Reader:          buf,
+		Channels:        []string{"UJ0SX0G9X"},
+		ThreadTimestamp: "",
+	}
+	var slackFile *slack.File
+	var err error
+	slackFile, err = api.UploadFile(params)
+
+	if err == nil {
+		fmt.Println(slackFile)
+	} else {
+		fmt.Printf("Error:%+v", err)
+	}
+}
