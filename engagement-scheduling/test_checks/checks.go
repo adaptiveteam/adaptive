ebdd1792f246aaee9977ac7b9096c1f2e2a193d2
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-05-08 14:04:53 +0300

    Fix scheduling tests

--- engagement-scheduling/test_checks/checks.go
+++ engagement-scheduling/test_checks/checks.go
@@ -1,13 +1,11 @@
 package test_checks
 
 import (
-	"github.com/adaptiveteam/adaptive/business-time"
+	adaptive_checks "github.com/adaptiveteam/adaptive/adaptive-checks"
+	business_time "github.com/adaptiveteam/adaptive/business-time"
 	"github.com/adaptiveteam/adaptive/daos/common"
-	"github.com/adaptiveteam/adaptive/adaptive-checks"
 )
 
-var SomeTrueAndSomeFalseTestProfile = adaptive_checks.SomeTrueAndSomeFalseTestProfile
-
-var ConstructTrueProfile adaptive_checks.TypedProfileConstructor = func (conn common.DynamoDBConnection, userID string, date business_time.Date) adaptive_checks.TypedProfile {
-	return SomeTrueAndSomeFalseTestProfile
+var ConstructTestProfile adaptive_checks.TypedProfileConstructor = func(conn common.DynamoDBConnection, userID string, date business_time.Date) adaptive_checks.TypedProfile {
+	return adaptive_checks.SomeTrueAndSomeFalseTestProfile
 }
