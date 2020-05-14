03d9616aac890dbfa05b8e3d11f188e47420b821
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-05-14 14:33:21 +0300

    Fix absent rule access

--- terraform-modules/adaptive-lambda-s3/schedule.tf
+++ terraform-modules/adaptive-lambda-s3/schedule.tf
@@ -8,10 +8,10 @@ resource "aws_cloudwatch_event_rule" "rule" {
 
 resource "aws_cloudwatch_event_target" "target" {
   count      = var.schedule ? 1 : 0
-  arn        = concat(aws_lambda_function.lambda.*.arn)[0]
-  rule       = aws_cloudwatch_event_rule.rule[0].name
+  arn        = concat(aws_lambda_function.lambda.*.arn, ["unavailable1"])[0]
+  rule       = concat(aws_cloudwatch_event_rule.rule.*.name, ["rule is absent"])[0]
   input      = var.schedule_invoke_json
-  target_id  = concat(aws_lambda_function.lambda.*.function_name)[0]
+  target_id  = concat(aws_lambda_function.lambda.*.function_name, ["unavailable2"])[0]
   depends_on = [aws_lambda_function.lambda]
 }
 
@@ -19,7 +19,7 @@ resource "aws_lambda_permission" "cloudwatch_invoke_lambda" {
   count         = var.schedule ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = concat(aws_lambda_function.lambda.*.arn)[0]
+  function_name = concat(aws_lambda_function.lambda.*.arn, ["unavailable3"])[0]
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.rule[0].arn
+  source_arn    = concat(aws_cloudwatch_event_rule.rule.*.arn,["rule unavailable3"])[0]
 }
